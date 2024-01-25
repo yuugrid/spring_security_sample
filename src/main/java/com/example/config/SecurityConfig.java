8a9f1b9e48a2a3b5474b8819fd5e04fa0406d2c8
yuugrid <152932000+yuugrid@users.noreply.github.com>
2024-01-25 21:01:39 +0900

    spring bootアプリケーション実行の際にエラー発生

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -1,5 +1,41 @@
 package com.example.config;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+    	http.authorizeRequests()
+    		.antMatchers("/loginForm").permitAll()
+    		.anyRequest().authenticated();
+    	
+    	http.formLogin()
+    		.loginProcessingUrl("/login")
+    		.loginPage("/loginForm")
+    		.usernameParameter("email")
+    		.passwordParameter("password")
+    		.defaultSuccessUrl("/home", true)
+    		.failureUrl("/loginForm?error");
+    	
+    	http.logout()
+    		.logoutUrl("/logout")
+    		.logoutSuccessUrl("/loginForm");
+    	
+        http.authorizeRequests()
+        	.antMatchers("/loginForm").permitAll()
+        	.antMatchers("/admin").hasAuthority("ADMIN")
+        	.anyRequest().authenticated();		
+    }
+    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
--- src/main/java/com/example/service/LoginUser.java
+++ src/main/java/com/example/service/LoginUser.java
@@ -1,5 +1,60 @@
 package com.example.service;
 
-public class LoginUser {
+import java.util.Collection;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.example.entity.User;
+
+public class LoginUser implements UserDetails {
+
+    private final User user;
+
+    public LoginUser(User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return this.user;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.user.getEmail();
+    }
+    
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+    	if(this.user.getRole().equals("管理者")) {
+    		return AuthorityUtils.createAuthorityList("ADMIN", "GENERAL");
+    	}
+    	return AuthorityUtils.createAuthorityList("GENARAL");
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
