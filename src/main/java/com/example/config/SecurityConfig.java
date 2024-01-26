0365d001cdd04f0fa2d0628e7ae68e248c605c36
yuugrid <152932000+yuugrid@users.noreply.github.com>
2024-01-26 11:26:35 +0900

    Lesson3実装完了

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -12,9 +12,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-    	http.authorizeRequests()
-    		.antMatchers("/loginForm").permitAll()
-    		.anyRequest().authenticated();
     	
     	http.formLogin()
     		.loginProcessingUrl("/login")
